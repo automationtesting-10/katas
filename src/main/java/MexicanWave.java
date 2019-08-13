207adabd4746ca20a0c4e0dca94ea1af79ac89f2
Cxristian <cxristian.lujan05@gmail.com>
2019-08-13 18:41:49 -0400

    create class MexicanWave

---
+++ src/main/java/MexicanWave.java
@@ -0,0 +1,48 @@
+
+/**
+ * MexicanWave class.
+ *
+ * @author Cristian Lujan
+ * @version 0.0.1
+ */
+class MexicanWave {
+
+    /**
+     * Create array wave.
+     * @param seatString input string
+     * @return wave
+     */
+    public static String[] wave(String seatString) {
+
+        final int seatCount = countSeats(seatString);
+        String[] wave = new String[seatCount];
+
+        for (int waveIndex = 0, seatIndex = 0; waveIndex < wave.length; waveIndex++, seatIndex++) {
+            char seatCharWave = seatString.charAt(seatIndex);
+            while (Character.isWhitespace(seatCharWave)) {
+                seatCharWave = seatString.charAt(++seatIndex);
+            }
+            StringBuilder seatWaveStringBuilder = new StringBuilder(seatString);
+            seatWaveStringBuilder.setCharAt(seatIndex, Character.toUpperCase(seatCharWave));
+            wave[waveIndex] = seatWaveStringBuilder.toString();
+        }
+        return wave;
+    }
+
+    /**
+     * Return count of spaces white.
+     * @param seatString input string
+     * @return count of space white
+     */
+    private static int countSeats(String seatString) {
+        int whiteSpaceCount = (int) seatString.codePoints().filter(Character::isWhitespace).count();
+        return seatString.length() - whiteSpaceCount;
+    }
+
+    /**
+     * Constructor of class.
+     */
+    protected MexicanWave() {
+
+    }
+}
