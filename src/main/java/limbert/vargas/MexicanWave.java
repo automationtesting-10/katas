dc1c17c0112eb3e32f1eb81207118b98f87470e7
LimbertVargas <limbert.vargas@fundacion-jala.org>
2019-08-13 15:38:01 -0400

    Maxican Wave class and test

--- src/main/java/limbert/vargas/MexicanWave.java
+++ src/main/java/limbert/vargas/MexicanWave.java
@@ -1,22 +1,53 @@
-package LimbertVargas;
+package limbert.vargas;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MexicanWave {
-    public static String capitalize(String str, int i) {
-        String [] array = str.split("");
-        array[i] = array[i].toUpperCase();
+/**
+ * The wave (known as the Mexican wave in the English-speaking world outside North America) is an example of metachronal
+ * rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms.
+ * Immediately upon stretching to full height, the spectator returns to the usual seated position.
+ * The result is a wave of standing spectators that travels through the crowd, even though individual spectators never
+ * move away from their seats. In many large arenas the crowd is seated in a contiguous circuit all the way around the
+ * sport field, and so the wave is able to travel continuously around the arena; in discontiguous seating arrangements,
+ * the wave can instead reflect back and forth through the crowd. When the gap in seating is narrow, the wave can
+ * sometimes pass through it. Usually only one wave crest will be present at any given time in an arena, although
+ * simultaneous, counter-rotating waves have been produced. (Source Wikipedia).
+ */
+public final class MexicanWave {
+
+    /**
+     * This is the constructor of the MexicanWave class.
+     */
+    private MexicanWave() {
+    }
+
+    /**
+     * This method analize all the enter words.
+     *
+     * @param str is the word.
+     * @param index scroll the index of the Uppercase letter.
+     * @return a string for analize the space.
+     */
+    public static String capitalize(String str, int index) {
+        String[] array = str.split("");
+        array[index] = array[index].toUpperCase();
         return String.join("", array);
     }
 
+    /**
+     * This method analize all teh words to enter in the Tests.
+     * @param str the word.
+     * @return a strign with uppercase letter scrolling.
+     */
     public static String[] wave(String str) {
         int size = str.length();
         List<String> result = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            String temp = capitalize(str,i);
-            if(!temp.equals(str))
+        for (int index = 0; index < size; index++) {
+            String temp = capitalize(str, index);
+            if (!temp.equals(str)) {
                 result.add(temp);
+            }
         }
         return result.stream().toArray(String[]::new);
     }
--- src/test/java/limbert/vargas/MexicanWaveTest.java
+++ src/test/java/limbert/vargas/MexicanWaveTest.java
@@ -1,4 +1,4 @@
-package LimbertVargas;
+package limbert.vargas;
 
 import org.junit.Test;
 
@@ -6,50 +6,81 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
+/**
+ * This is the test of MexicanWave class.
+ */
 public class MexicanWaveTest {
+
+    /**
+     * First test.
+     */
     @Test
     public void basicTest1() {
         System.out.println("Testing for 'a       b    '");
-        String[] result = new String[] { "A       b    ", "a       B    " };
-        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave("a       b    "));
+        String[] result = new String[]{"A       b    ", "a       B    "};
+        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave(
+                "a       b    "));
     }
 
+    /**
+     * Second test.
+     */
     @Test
     public void basicTest2() {
         System.out.println("Testing for 'this is a few words'");
-        String[] result = new String[] { "This is a few words", "tHis is a few words", "thIs is a few words", "thiS is a few words", "this Is a few words", "this iS a few words", "this is A few words", "this is a Few words", "this is a fEw words", "this is a feW words", "this is a few Words", "this is a few wOrds", "this is a few woRds", "this is a few worDs", "this is a few wordS" };
-        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave("this is a few words"));
+        String[] result = new String[]{"This is a few words", "tHis is a few words", "thIs is a few words",
+                "thiS is a few words", "this Is a few words", "this iS a few words", "this is A few words",
+                "this is a Few words", "this is a fEw words", "this is a feW words", "this is a few Words",
+                "this is a few wOrds", "this is a few woRds", "this is a few worDs", "this is a few wordS"};
+        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave(
+                "this is a few words"));
     }
 
+    /**
+     * Third test.
+     */
     @Test
     public void basicTest3() {
         System.out.println("Testing for ''");
-        String[] result = new String[] { };
-        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave(""));
+        String[] result = new String[]{};
+        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result,
+                MexicanWave.wave(""));
     }
 
+    /**
+     * Fourth test.
+     */
     @Test
     public void basicTest4() {
         System.out.println("Testing for ' gap '");
-        String[] result = new String[] { " Gap ", " gAp ", " gaP " };
-        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave(" gap "));
+        String[] result = new String[]{" Gap ", " gAp ", " gaP "};
+        assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result,
+                MexicanWave.wave(" gap "));
     }
 
+    /**
+     * Fifth Test.
+     */
     @Test
     public void randomTest() {
         for (int i = 0; i <= 200; i++) {
             String word = getWord();
             System.out.println("Testing for '" + word + "'");
             String[] result = wave2(word);
-            assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result, MexicanWave.wave(word));
+            assertArrayEquals("it should return '" + Arrays.toString(result) + "'", result,
+                    MexicanWave.wave(word));
         }
     }
 
     private Random rand = new Random();
     private String letters = "abcd efghi jklmno pqrstu vwxyz";
 
+    /**
+     * This method scroll letters in the word entering.
+     * @return a builder.toString().
+     */
     private String getWord() {
         StringBuilder builder = new StringBuilder();
 
@@ -60,10 +91,16 @@ public class MexicanWaveTest {
         return builder.toString();
     }
 
+    /**
+     * This method scroll letters in the second word entering.
+     * @param str word in string type.
+     * @return a IntStream.
+     */
     private String[] wave2(String str) {
         return IntStream
                 .range(0, str.length())
-                .mapToObj(x -> new StringBuilder(str).replace(x, x+1, String.valueOf(str.charAt(x)).toUpperCase()).toString())
+                .mapToObj(x -> new StringBuilder(str).replace(x, x + 1, String.valueOf(str.charAt(x))
+                        .toUpperCase()).toString())
                 .filter(x -> !x.equals(str))
                 .toArray(String[]::new);
     }
